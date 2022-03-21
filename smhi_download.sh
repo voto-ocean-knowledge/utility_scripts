b9776723440d53872ac9086243b08e9612e7568a
Callum Rollo <c.rollo@outlook.com>
2022-03-21 17:37:13 +0000

    point to api documentation

--- smhi_download.sh
+++ smhi_download.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+# bash script to download NEMO model data from SMHI
+# API documentation at https://opendata.smhi.se/apidocs/grid/
+
 today=$(date +%F)
 yesterday=$(date -d "yesterday" '+%F')
 geturl="opendata-download-grid-archive.smhi.se/feed/8/data?from=${yesterday}T00:00:00.000Z&to=${today}T00:01:00.000Z"
