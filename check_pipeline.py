7ce745d95265c1e89f3609b92ee1c1e10284b88a
Callum Rollo <c.rollo@outlook.com>
2022-04-21 17:10:20 +0200

    add logging in pipeline check

--- check_pipeline.py
+++ check_pipeline.py
@@ -2,6 +2,7 @@
 This script checks that the other steps of the pipeline are running as expected.
 To monitor a file, add the path to it and part of its expected last line to the files_dict
 """
+import datetime
 import subprocess
 files_dict = {"/data/log/rsync_nrt.log": "total size is",
               "/data/log/pyglider_nrt.log": "Finished nrt processing",
@@ -18,9 +19,15 @@ def get_last_line(file):
 
 
 if __name__ == '__main__':
+    print(f"{datetime.datetime.now} start check")
+    errors = False
     for filename, string in files_dict.items():
+        print(f"Check {filename}")
         final_line = get_last_line(filename)
         if string not in final_line:
+            errors = True
             msg = f"failed process: {filename} ends in {final_line}"
             subprocess.check_call(['/usr/bin/bash', "/home/pipeline/utility_scripts/send.sh", msg])
+        if errors:
+            print(f"{datetime.datetime.now()} failure detected in pipeline ")
             
