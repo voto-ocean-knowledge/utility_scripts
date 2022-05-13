74dfb3c5cf290d254306f26124ce122ac61c6b96
Callum Rollo <c.rollo@outlook.com>
2022-05-13 15:14:18 +0200

    refactor geocode to immport

--- geocode.py
+++ geocode.py
@@ -22,9 +22,9 @@ def get_seas(gridfile):
     df_glider = df_glider.to_crs(df_helcom.crs)
     polygons_contains = gp.sjoin(df_helcom, df_glider, op='contains')
     basin_points = polygons_contains.level_2.values
-    if not basin_points:
-        return ""
     basin_counts = Counter(basin_points).most_common()
+    if not basin_counts:
+        return ""
     basins_ordered = [x[0] for x in basin_counts]
     basin_str = ", ".join(basins_ordered)
     return basin_str
@@ -33,13 +33,33 @@ def get_seas(gridfile):
 def nc_add_sea(nc_path, basin_str, tempfile):
     _log.info(f"working on {nc_path}")
     ds = xr.open_dataset(nc_path)
-    _log.info('read files successfully')
     meta = ds.attrs
     meta["basin"] = basin_str
     ds.attrs = meta
     ds.to_netcdf(tempfile)
     shutil.move(tempfile, nc_path)
-    _log.info("Successfully saved nc")
+
+
+def update_ncs(glider, mission, sub_dir):
+    root_dir = f"/data/data_l0_pyglider/{sub_dir}/SEA{str(glider)}/M{str(mission)}"
+    _log.info(f"add basin to ncs in {root_dir}")
+    nc_files = []
+    for sub in ('profiles', 'timeseries', 'gridfiles'):
+        nc_files.append(list(pathlib.Path(root_dir).glob(f"**/{sub}/*.nc")))
+    nc_files_flat = list(chain.from_iterable(nc_files))
+    if not nc_files:
+        _log.error(f"no ncs found in path {root_dir}")
+    _log.info(f"Found {len(nc_files_flat)}")
+    gridfile_dir = pathlib.Path(
+        f"/data/data_l0_pyglider/{sub_dir}/SEA{str(glider)}/M{str(mission)}/gridfiles")
+    gridfile = list(gridfile_dir.glob("*.nc"))[0]
+    basin = get_seas(gridfile)
+    _log.info(f"Basin: {basin}")
+    for nc in nc_files_flat:
+        temp_nc = pathlib.Path("/tmp") / pathlib.Path(nc).name
+        nc_add_sea(nc, basin, temp_nc)
+    _log.info(f"Updated all ncs in {root_dir}")
+    _log.info("Success! added basin to all ncs")
 
 
 if __name__ == '__main__':
@@ -56,27 +76,9 @@ if __name__ == '__main__':
                         format='%(asctime)s %(levelname)-8s %(message)s',
                         level=logging.INFO,
                         datefmt='%Y-%m-%d %H:%M:%S')
-    types = []
-    if args.kind != 'sub':
-        sub_dir = "nrt"
+    if args.kind == 'sub':
+        sub_dir_in = "nrt"
     else:
-        sub_dir = 'complete_mission'
-        
-    root_dir = f"/data/data_l0_pyglider/{sub_dir}/SEA{str(args.glider)}/M{str(args.mission)}"
-    _log.info(f"add basin to ncs in {root_dir}")
-    nc_files = []
-    for sub in ('profiles', 'timeseries', 'gridfiles'):
-        nc_files.append(list(pathlib.Path(root_dir).glob(f"**/{sub}/*.nc")))
-    nc_files_flat = list(chain.from_iterable(nc_files))
-    if not nc_files:
-        _log.error(f"no ncs found in path {root_dir}")
-    _log.info(f"Found {len(nc_files_flat)}")
-    gridfile_dir = pathlib.Path(f"/data/data_l0_pyglider/{sub_dir}/SEA{str(args.glider)}/M{str(args.mission)}/gridfiles")
-    gridfile = list(gridfile_dir.glob("*.nc"))[0]
-    basin = get_seas(gridfile)
-    _log.info(f"Basin: {basin}")
-    for nc in nc_files_flat:
-        temp_nc = pathlib.Path("/tmp") / pathlib.Path(nc).name
-        nc_add_sea(nc, basin, temp_nc)
-    _log.info(f"Updated all ncs in {root_dir}")
-    _log.info("Success! added basin to all ncs")
+        sub_dir_in = 'complete_mission'
+    update_ncs(args.glider, args.mission, sub_dir_in)
+    
