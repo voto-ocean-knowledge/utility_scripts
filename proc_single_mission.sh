1b2e49cf52959bad9a265d18063345c7563c285f
Callum Rollo <c.rollo@outlook.com>
2022-05-13 14:05:52 +0200

    use new environement

--- proc_single_mission.sh
+++ proc_single_mission.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # script to process a single mission in the bakcground and log the progress
-nohup /data/miniconda/envs/pyglider/bin/python /home/pipeline/utility_scripts/pyglider_single_mission.py $1 $2 raw > /data/log/complete_mission/error_complete_SEA$1_$2.log 2>&1 &
+nohup /data/miniconda/envs/process/bin/python /home/pipeline/utility_scripts/pyglider_single_mission.py $1 $2 raw > /data/log/complete_mission/error_complete_SEA$1_$2.log 2>&1 &
 
