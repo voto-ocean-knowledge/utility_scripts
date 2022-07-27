2786708cb98a1dd0bc4365e64d58b0be3e7884a9
Callum Rollo <c.rollo@outlook.com>
2022-07-27 18:19:10 +0200

    correct path for clean

--- clean_mission.sh
+++ clean_mission.sh
@@ -3,7 +3,7 @@
 glider=$1
 mission=$2
 echo clean SEA$glider mission $mission data
-missionsub="proc_SEA${glider}_M${mission}_sub"
+missionsub="SEA${glider}_M${mission}_sub"
 echo ""
 echo raw sub data
 ls -d1 /data/tmp/subs/raw_$missionsub*
