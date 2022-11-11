a2c043bb992fa3102083bb2feb813f33f41ad175
Callum Rollo <c.rollo@outlook.com>
2022-11-11 10:26:43 +0100

    fix profile index and set int for qc and counts

--- process_pyglider.py
+++ process_pyglider.py
@@ -3,6 +3,7 @@ import sys
 import pathlib
 import shutil
 import yaml
+import numpy as np
 import polars as pl
 import xarray as xr
 from geocode import get_seas_merged_nav_nc
@@ -24,6 +25,45 @@ def safe_delete(directories):
     for directory in directories:
         if pathlib.Path.exists(pathlib.Path.absolute(script_dir / directory)):
             shutil.rmtree(directory)
+
+
+def set_profile_numbers(ds):
+    ds["dive_num"] = ds["dive_num"].astype(int)
+    df = ds.to_pandas()
+    df["profile_index"] = 0
+    deepest_points = []
+    dive_nums = np.unique(df.dive_num)
+    for num in dive_nums:
+        df_dive = df[df.dive_num == num]
+        deep_inflect = df_dive[df_dive.pressure == df_dive.pressure.max()].index.values[0]
+        deepest_points.append(deep_inflect)
+
+    previous_deep_inflect = deepest_points[0]
+    df.loc[df.index[0]:previous_deep_inflect, "profile_index"] = 0
+    num = 0
+    for i, deep_inflect in enumerate(deepest_points[1:]):
+        num = i + 1
+        df_deep_to_deep = df.loc[previous_deep_inflect: deep_inflect]
+        shallow_inflect = df_deep_to_deep[df_deep_to_deep.pressure == df_deep_to_deep.pressure.min()].index.values[0]
+        df.loc[previous_deep_inflect:shallow_inflect, "profile_index"] = num * 2 - 1
+        df.loc[shallow_inflect:deep_inflect, "profile_index"] = num * 2
+        previous_deep_inflect = deep_inflect
+    df.loc[previous_deep_inflect:df.index[-1], "profile_index"] = num * 2 + 1
+
+    df["profile_direction"] = -1
+    df.loc[df.profile_index % 2 == 0, "profile_direction"] = 1
+    ds["profile_index"] = df.dive_num.copy()
+    ds["profile_direction"] = df.dive_num.copy()
+    ds["profile_index"].values = df.profile_index
+    ds["profile_direction"].values = df.profile_direction
+    ds["profile_index"].attrs = {'long_name': 'profile index',
+                                 'units': '1',
+                                 'sources': 'pressure, time, dive_num'}
+    ds["profile_direction"].attrs = {'long_name': 'profile direction',
+                                     'units': '1',
+                                     'sources': 'pressure, time, dive_num',
+                                     'comment': '-1 = ascending, 1 = descending'}
+    return ds
 
 
 def proc_pyglider_l0(glider, mission, kind, input_dir, output_dir, steps=()):
@@ -80,12 +120,16 @@ def proc_pyglider_l0(glider, mission, kind, input_dir, output_dir, steps=()):
         outname = seaexplorer.raw_to_L0timeseries(rawncdir, l0tsdir, deploymentyaml, kind=kind)
         tempfile = f"/data/tmp/SEA{glider}_M{mission}"
         int_vars = ["angular_cmd", "ballast_cmd", "linear_cmd", "nav_state", "security_level", "dive_num",
-                    "desired_heading"]
+                    "desired_heading", "chlorophyll_raw", "phycocyanin_raw", "turbidity_raw"]
         ds = xr.open_dataset(outname)
         ds = flagger(ds)
-        for var in list(ds):
-            if var in int_vars or var[:-2:] == "qc":
+        ds_variables = list(ds)
+        for var in ds_variables:
+            if var in int_vars or var[-2:] == "qc":
+                print(var)
                 ds[var] = ds[var].astype(int)
+                print(type(ds[var].values[0]))
+        ds = set_profile_numbers(ds)
         ds.to_netcdf(tempfile, encoding={'time': {'units': 'seconds since 1970-01-01T00:00:00Z'}})
         shutil.move(tempfile, outname)
         ds = apply_flags(ds)
