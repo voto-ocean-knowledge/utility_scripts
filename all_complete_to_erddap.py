9d3b6cc5b8f561e4202c4378c207cf3861d9af88
Callum Rollo <c.rollo@outlook.com>
2022-11-17 15:29:51 +0100

    rsync all complete to erddap

---
+++ all_complete_to_erddap.py
@@ -0,0 +1,31 @@
+import os
+import sys
+import pathlib
+import pandas as pd
+import logging
+import subprocess
+
+script_dir = pathlib.Path(__file__).parent.absolute()
+parent_dir = script_dir.parents[0]
+sys.path.append(str(script_dir))
+os.chdir(script_dir)
+
+_log = logging.getLogger(__name__)
+
+
+if __name__ == '__main__':
+    logf = f'/data/log/complete_mission_to_erddap.log'
+    logging.basicConfig(filename=logf,
+                        filemode='a',
+                        format='%(asctime)s %(levelname)-8s %(message)s',
+                        level=logging.INFO,
+                        datefmt='%Y-%m-%d %H:%M:%S')
+    _log.info("Start send to erddap")
+    df_reprocess = pd.read_csv('/home/pipeline/reprocess.csv', parse_dates=["proc_time"])
+    df_reprocess.sort_values("proc_time", inplace=True)
+
+    for i, row in df_reprocess.iterrows():
+        glider, mission, proc = row.glider, row.mission, row.proc_time
+        _log.info(f"Send SEA{glider} M{mission}")
+        subprocess.check_call(['/usr/bin/bash', "/home/pipeline/utility_scripts/send_to_pipeline.sh", str(glider), str(mission)])
+    _log.info(f"Complete send to erddap")
--- pyglider_all_complete_missions.py
+++ pyglider_all_complete_missions.py
@@ -164,6 +164,4 @@ if __name__ == '__main__':
         df_reprocess.at[i, "duration"] = datetime.datetime.now() - start
         df_reprocess.to_csv('/home/pipeline/reprocess.csv', index=False)
         _log.info("Finished add to database")
-        subprocess.check_call(['/usr/bin/bash', "/home/pipeline/utility_scripts/send_to_pipeline.sh", str(glider), str(mission)])
-        _log.info("Sent file to erddap")
         _log.info(f"Complete Reprocessing SEA{glider} M{mission}")
