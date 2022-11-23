e22a938b5ed6f27f43473a8b22d365a739a41030
Callum Rollo <c.rollo@outlook.com>
2022-11-23 10:38:16 +0100

    correct send all complete

--- all_complete_to_erddap.py
+++ all_complete_to_erddap.py
@@ -23,10 +23,12 @@ if __name__ == '__main__':
     _log.info("Start send to erddap")
     df_reprocess = pd.read_csv('/home/pipeline/reprocess.csv', parse_dates=["proc_time"])
     df_reprocess.sort_values("proc_time", inplace=True)
+    df_reprocess.reset_index(inplace=True)
     total = len(df_reprocess)
     _log.info(f"will send {total} files to erddap")
     for i, row in df_reprocess.iterrows():
         glider, mission, proc = row.glider, row.mission, row.proc_time
+        print(f"Will send file {i}/{total}: SEA{glider} M{mission}")
         _log.info(f"Send file {i}/{total}: SEA{glider} M{mission}")
         subprocess.check_call(['/usr/bin/bash', "/home/pipeline/utility_scripts/send_to_pipeline.sh", str(glider), str(mission)])
     _log.info(f"Complete send to erddap")
