62aca57c6a6792c4b0814ba2f3ec7a69c76ae1b3
Callum Rollo <c.rollo@outlook.com>
2023-01-26 16:19:46 +0100

    wakey to work on cronjob

---
+++ waker.sh
@@ -0,0 +1,3 @@
+#!/bin/zsh
+export XDG_RUNTIME_DIR="/run/user/1000"
+/home/callum/anaconda3/envs/process/bin/python /home/callum/Documents/data-flow/raw-to-nc/utility_scripts/wakeywakey.py
--- wakeywakey.py
+++ wakeywakey.py
@@ -15,6 +15,8 @@ from datetime import datetime
 from pathlib import Path
 import os
 import sys
+import logging
+_log = logging.getLogger(__name__)
 script_dir = Path(__file__).parent.absolute()
 sys.path.append(str(script_dir))
 os.chdir(script_dir)
@@ -24,12 +26,15 @@ with open("email_secrets.json") as json_file:
 
 
 def sounds(text):
+    _log.info(f"Will play {text}")
     play(AudioSegment.from_mp3('al.mp3'))
+    _log.debug("played first soung")
     glider, mission, __, __, alarm_code = text.split(" ")
     message = f"sea {glider[4:-1]} has alarmed with code {alarm_code[6:-1]}. Get up"
     speech = gTTS(text=message, lang="en", tld='com.au')
     speech.save("message.mp3")
     play(AudioSegment.from_mp3('message.mp3'))
+    _log.debug("played full message")
 
 
 def read_email_from_gmail():
@@ -76,12 +81,13 @@ def read_email_from_gmail():
 
     # Exit if no new emails
     if not unread_emails:
-        with open('email_log.txt', 'a') as f:
-            f.write(str(datetime.now()) + ' no new mail' + '\n')
-        exit(0)
+        _log.info("No new mail")
+        return
+    _log.debug("New emails")
 
     # Check new emails
     for i in unread_emails:
+        _log.debug(f"open mail {i}")
         result, data = mail.fetch(str(i), '(RFC822)')
         for response_part in data:
             if isinstance(response_part, tuple):
@@ -92,8 +98,15 @@ def read_email_from_gmail():
                 email_from = msg['from']
                 # If email is from alseamar and subject contains ALARM, make some noise
                 if "administrateur@alseamar-cloud.com" in email_from or "calglider" in email_from and "ALARM" in email_subject:
+                    _log.warning(f"alarm {email_subject}")
                     sounds(email_subject)
 
 
 if __name__ == '__main__':
+    logf = f'email.log'
+    logging.basicConfig(filename=logf,
+                        filemode='a',
+                        format='%(asctime)s %(levelname)-8s %(message)s',
+                        level=logging.INFO,
+                        datefmt='%Y-%m-%d %H:%M:%S')
     read_email_from_gmail()
