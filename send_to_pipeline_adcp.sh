acdfd8acdd1277b48d0f99981d6a6c9e9099e709
Callum Rollo <c.rollo@outlook.com>
2023-01-27 13:42:21 +0100

    scripts to send adcp datasets

---
+++ send_to_pipeline_adcp.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Utility to rsync completed mission data to ERDDAP server
+glider=$1
+mission=$2
+echo send SEA$glider mission $mission data to pipeline
+tgtdir=/media/data/data_dir/complete_mission/SEA$glider/M$mission/ADCP
+echo make directory on target if it does not already exist
+ssh erddapdata@13.51.101.57 mkdir -p $tgtdir
+echo ""
+echo rsync data
+rsync -v  /data/data_l0_pyglider/complete_mission/SEA$glider/M$mission/ADCP/adcp.nc  "erddapdata@13.51.101.57:$tgtdir"
+echo Finished
---
+++ upload_adcp.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Utility to rsync raw mission data to pipeline
+glider=$1
+mission=$2
+adcpfile=$3
+echo send SEA$glider mission $mission adcp data to pipeline
+tgtdir=/data/data_raw/complete_mission/SEA$glider/M$mission/ADCP
+echo make directory on target if it does not already exist
+ssh pipeline@16.170.107.21 mkdir -p $tgtdir
+echo ""
+echo rsync data
+rsync $adcpfile "pipeline@16.170.107.21:$tgtdir"
+echo Finished
