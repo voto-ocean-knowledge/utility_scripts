5caf405bd9921daf2f531846331afe701a568453
Callum Rollo <c.rollo@outlook.com>
2023-02-06 12:10:57 +0100

    start to process backscatter data

---
+++ post_process_dataset.py
@@ -0,0 +1,69 @@
+import numpy as np
+import re
+import argparse
+from post_process_optics import betasw_ZHH2009
+import logging
+import xarray as xr
+from pathlib import Path
+import shutil
+
+from utilities import encode_times
+
+_log = logging.getLogger(__name__)
+
+
+def calculate_bbp(ds, beam_angle=117):
+    # https://oceanobservatories.org/wp-content/uploads/2015/10/1341-00540_Data_Product_SPEC_FLUBSCT_OOI.pdf
+    temperature = ds["temperature"].values
+    salinity = ds["salinity"].values
+    beta_total = ds["backscatter"].values
+    backscatter_str = ds.backscatter.attrs["standard_name"]
+    wavelength = int(re.findall(r'\d+', backscatter_str)[0])
+    beta_sw, __, __ = betasw_ZHH2009(temperature, salinity, wavelength, beam_angle)
+    beta_p = beta_total - beta_sw
+    if beam_angle == 117:
+        chi_p = 1.08  # For 117* angle (Sullivan & Twardowski, 2009)
+    elif beam_angle == 140:
+        chi_p = 1.17  # For 140* angle (Sullivan & Twardowski, 2009)
+    else:
+        _log.error(f"Incompatible beam_angle. Allowed values are 117 or 140")
+        return
+    bbp_val = 2 * np.pi * chi_p * beta_p  # in m-1
+    bbp = ds["backscatter"].copy()
+    bbp.values = bbp_val
+    bbp.attrs = {"units": "m^{-1}"}
+    ds["particulate backscattering coefficient"] = bbp
+    return ds
+
+
+def post_process(ds):
+    if "backscatter" in list(ds):
+        ds = calculate_bbp(ds)
+    return ds
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Combine ADCP data with glider data')
+    parser.add_argument('glider', type=int, help='glider number, e.g. 70')
+    parser.add_argument('mission', type=int, help='Mission number, e.g. 23')
+    parser.add_argument('kind', type=str, help='Kind of input, must be raw or sub')
+    args = parser.parse_args()
+    if args.kind not in ['raw', 'sub']:
+        raise ValueError('kind must be raw or sub')
+    logf = f'/data/log/complete_mission/post_process_SEA{str(args.glider)}_M{str(args.mission)}.log'
+    logging.basicConfig(filename=logf,
+                        filemode='w',
+                        format='%(asctime)s %(levelname)-8s %(message)s',
+                        level=logging.INFO,
+                        datefmt='%Y-%m-%d %H:%M:%S')
+    if args.kind == "raw":
+        mtype = "complete_mission"
+    else:
+        mtype = "nrt"
+    nc_path = Path(f"/data/data_l0_pyglider/{mtype}/SEA{args.glider}/M{args.mission}/timeseries/mission_timeseries.nc")
+    ds_in = xr.open_dataset(nc_path)
+    post_process(ds_in)
+    ds_in = encode_times(ds_in)
+    tempfile = f"/data/tmp/SEA{args.glider}_M{args.mission}"
+    ds_in.to_netcdf(tempfile)
+    shutil.move(tempfile, str(nc_path))
