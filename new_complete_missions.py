cc772e901b3f636963904fa941a6e7c99ebee84b
Callum Rollo <c.rollo@outlook.com>
2023-06-29 11:27:32 +0200

    fix paths for new mission proc

--- new_complete_missions.py
+++ new_complete_missions.py
@@ -20,35 +20,41 @@ def main():
     df_reprocess.sort_values("proc_time", inplace=True)
     _log.info(f"start length {len(df_reprocess)}")
     glider_paths = list(pathlib.Path("/data/data_raw/complete_mission").glob("SEA*"))
-    if len(glider_paths) == len(df_reprocess):
+    glider_paths_good = []
+    for path in glider_paths:
+        if "SEA57" in str(path):
+            continue
+        mission_paths = path.glob("M*")
+
+        glider_paths_good.append(mission_paths)
+    glider_paths_good = [item for sublist in glider_paths_good for item in sublist]
+    if len(glider_paths_good) == len(df_reprocess):
         _log.info("No new missions to process")
         return
-    for glider_path in glider_paths:
-        mission_paths = glider_path.glob("M*")
-        for mission_path in mission_paths:
-            glider = int(glider_path.parts[-1][3:])
-            mission = int(mission_path.parts[-1][1:])
-            a = [np.logical_and(df_reprocess.glider == glider, df_reprocess.mission == mission)]
-            if not sum(sum(a)):
-                _log.warning(f"new mission {mission_path}")
-                args = types.SimpleNamespace()
-                args.glider = glider
-                args.mission = mission
-                args.type = "raw"
-                process(args)
-                nc_file = list((pathlib.Path(f"/data/data_l0_pyglider/complete_mission/SEA{glider}/M{mission}/timeseries")).glob('*.nc'))[0]
-                nc_time = nc_file.lstat().st_mtime
-                nc_time = datetime.datetime.fromtimestamp(nc_time)
-                new_row = pd.DataFrame({"glider": glider, "mission": mission,
-                                        "proc_time": nc_time, "duration": datetime.timedelta(minutes=20)},
-                                       index=[len(df_reprocess)])
-                df_reprocess = pd.concat((df_reprocess, new_row))
+    for mission_path in glider_paths_good:
+        glider = int(mission_path.parts[-2][3:])
+        mission = int(mission_path.parts[-1][1:])
+        a = [np.logical_and(df_reprocess.glider == glider, df_reprocess.mission == mission)]
+        if not sum(sum(a)):
+            _log.warning(f"new mission {mission_path}")
+            args = types.SimpleNamespace()
+            args.glider = glider
+            args.mission = mission
+            args.type = "raw"
+            process(args)
+            nc_file = list((pathlib.Path(f"/data/data_l0_pyglider/complete_mission/SEA{glider}/M{mission}/timeseries")).glob('*.nc'))[0]
+            nc_time = nc_file.lstat().st_mtime
+            nc_time = datetime.datetime.fromtimestamp(nc_time)
+            new_row = pd.DataFrame({"glider": glider, "mission": mission,
+                                    "proc_time": nc_time, "duration": datetime.timedelta(minutes=20)},
+                                   index=[len(df_reprocess)])
+            df_reprocess = pd.concat((df_reprocess, new_row))
     _log.info(f"end length {len(df_reprocess)}")
     df_reprocess["gm"] = df_reprocess.glider * 10000 + df_reprocess.mission
     df_reprocess = df_reprocess.groupby("gm").first()
     df_reprocess.sort_values("proc_time", inplace=True)
-    _log.info("complete")
 
 
 if __name__ == '__main__':
     main()
+    _log.info("Complete")
