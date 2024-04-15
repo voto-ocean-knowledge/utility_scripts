2fb174fe475db404093baf9bbeab268c4997294d
Callum Rollo <c.rollo@outlook.com>
2024-04-15 13:28:01 +0200

    send allow linebreaks

--- send.sh
+++ send.sh
@@ -1,3 +1,3 @@
 # Utility script to send message passed to callum
-cmd=$(echo $1 | NULLMAILER_NAME="Pipeline monitor" mail -s $2 $3)
+cmd=$(echo -e "$1" | NULLMAILER_NAME="Pipeline monitor" mail -s $2 $3)
 
