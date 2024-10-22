26573072b800033958cd5f70300217563fcb61fb
Callum Rollo <c.rollo@outlook.com>
2024-10-22 15:39:37 +0200

    [REF] reorganise into directories

--- clean_mission.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Utility to clean up sub folders after processing
-echo removing rawnc files
-rm -rf /data/data_l0_pyglider/complete_mission/SEA$glider/M$mission/rawnc
--- complete_to_nrt.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# script to convert a single mission in backkground
-nohup /data/miniconda/envs/process/bin/python /home/pipeline/utility_scripts/pyglider_nrt_from_complete.py $1 $2
