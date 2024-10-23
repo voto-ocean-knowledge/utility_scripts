883093f0268dc68524c2bd7b032e7f2b81b62e69
Callum Rollo <c.rollo@outlook.com>
2024-10-23 10:33:00 +0200

    [REF] move pipeline

--- votoutils/pipeline/proc_single_mission.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# script to process a single mission in the background and log the progress
-nohup /home/pipeline/micromamba/envs/process/bin/python /home/pipeline/utility_scripts/pyglider_single_mission.py $1 $2 > /data/log/complete_mission/error_complete_SEA$1_M$2.log 2>&1 &
-
